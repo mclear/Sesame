8f8d749da7dc107109d17b5a3b0e2f6d7df2ff6b
steelmaker <a.aktanorovich@scout-gps.ru>
2017-11-20 10:32:21 +0300

    run service after installation

--- Service/NFCRingServiceHost/ProjectInstaller.cs
+++ Service/NFCRingServiceHost/ProjectInstaller.cs
@@ -1,28 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Configuration.Install;
-using System.Linq;
+using System.Diagnostics;
 using System.ServiceProcess;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NFCRing.Service.Host
 {
     [RunInstaller(true)]
     public class ProjectInstaller : Installer
     {
-        private ServiceProcessInstaller process;
-        private ServiceInstaller service;
+        private readonly ServiceInstaller _serviceInstaller;
 
         public ProjectInstaller()
         {
-            process = new ServiceProcessInstaller();
-            process.Account = ServiceAccount.LocalSystem;
-            service = new ServiceInstaller();
-            service.ServiceName = "NFCRingService";
-            Installers.Add(process);
-            Installers.Add(service);
+            var serviceProcessInstaller = new ServiceProcessInstaller();
+            serviceProcessInstaller.Account = ServiceAccount.LocalSystem;
+            serviceProcessInstaller.AfterInstall += OnAfterInstall;
+            _serviceInstaller = new ServiceInstaller();
+            _serviceInstaller.ServiceName = "NFCRingService";
+            _serviceInstaller.Description = "Service for lock and unlock your computer by NFC Ring";
+            _serviceInstaller.StartType = ServiceStartMode.Automatic;
+            Installers.Add(serviceProcessInstaller);
+            Installers.Add(_serviceInstaller);
+        }
+
+        private void OnAfterInstall(object sender, InstallEventArgs e)
+        {
+            var processStartInfo = new ProcessStartInfo
+            {
+                WindowStyle = ProcessWindowStyle.Hidden,
+                FileName = "net.exe",
+                Arguments = "start " + _serviceInstaller.ServiceName
+            };
+
+            var process = new Process { StartInfo = processStartInfo };
+            process.Start();
         }
     }
 }
